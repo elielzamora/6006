77be0757b08bc4395019d0229dff424534953932
Eliel Zamora <elielzamora@users.noreply.github.com>
2016-08-17 21:06:25 -0400

    ignore more

--- lec4/heapsort.c
+++ lec4/heapsort.c
@@ -5,28 +5,44 @@
                 - Priority Queue ADT
                 - Heap functions
                 - Heapsort
-                
 */
 
 typedef struct {
         int * heap
         int size;
-        int cap;
 } Heap;
 typedeft struct{
-        //todo
-}
+        int * array
+        int size;
+} Array;
 typedef Heap PriorityQueue;
 
-int heapify (Heap heap, int i);
+int heapify (Heap h, int i);
 int makeHeap(int * array, int size);
-int getParent(Heap heap, int i);
-int getLeft(Heap heap int i);
-int getRight(Heap heap, int i);
+int parent(int i);
+int left(int i);
+int right(int i);
+int getParent(Heap h, int i);
+int getLeft(Heap h int i);
+int getRight(Heap h, int i);
+int assertMaxHeapProperty(Heap h);
+int assertSorted(Array a);
 Array heapsort(Heap heap);
 
 int main (int argc, char ** argv){
 
 
 
+}
+
+/** Definitions */
+
+int parent(int i){
+        return (i/2) -1
+}
+int left(int i){
+	return (i*2) -1
+}
+int right(int i){
+        return (i*2)
 }
